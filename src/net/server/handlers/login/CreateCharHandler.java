39b9a5b18289f6124fd9f32b3a6759d1bec6d429
Ryan Payton <ryantpaytong@gmail.com>
2024-04-21 13:30:03 -0400

    Added custom client check for invalid items during character creation

--- src/net/server/handlers/login/CreateCharHandler.java
+++ src/net/server/handlers/login/CreateCharHandler.java
@@ -34,13 +34,14 @@ import java.util.HashSet;
 import java.util.Set;
 
 public final class CreateCharHandler extends AbstractMaplePacketHandler {
+
     private final static Set<Integer> IDs = new HashSet<>(Arrays.asList(new Integer[]{
-            1302000, 1312004, 1322005, 1442079,                                         // Weapons
-            1040002, 1040006, 1040010, 1041002, 1041006, 1041010, 1041011, 1042167,     // Bottom
-            1060002, 1060006, 1061002, 1061008, 1062115,                                // Top
-            1072001, 1072005, 1072037, 1072038, 1072383,                                // Shoes
-            30000, 30010, 30020, 30030, 31000, 31040, 31050,                            // Hair
-            20000, 20001, 20002, 21000, 21001, 21002, 21201, 20401, 20402, 21700, 20100 // Face
+        1302000, 1312004, 1322005, 1442079,                                         // Weapons
+        1040002, 1040006, 1040010, 1041002, 1041006, 1041010, 1041011, 1042167,     // Bottom
+        1060002, 1060006, 1061002, 1061008, 1062115,                                // Top
+        1072001, 1072005, 1072037, 1072038, 1072383,                                // Shoes
+        30000, 30010, 30020, 30030, 31000, 31040, 31050,                            // Hair
+        20000, 20001, 20002, 21000, 21001, 21002, 21201, 20401, 20402, 21700, 20100 // Face
     }));
 
     private static boolean isLegal(Integer toCompare) {
@@ -52,20 +53,9 @@ public final class CreateCharHandler extends AbstractMaplePacketHandler {
         String name = slea.readMapleAsciiString();
         int job = slea.readInt();
         int face = slea.readInt();
-
-        int hair;
-        int hairstyle;
-        int haircolor;
-
-        if (ServerConstants.USE_CUSTOM_CLIENT) {
-            hair = slea.readInt();
-        } else {
-            hairstyle = slea.readInt();
-            haircolor = slea.readInt();
-
-            hair = hairstyle + haircolor;
-        }
-
+        int hairstyle = slea.readInt();
+        int haircolor = slea.readInt();
+        int hair = hairstyle + haircolor;
         int skin = slea.readInt();
         int top = slea.readInt();
         int bottom = slea.readInt();
@@ -73,17 +63,58 @@ public final class CreateCharHandler extends AbstractMaplePacketHandler {
         int weapon = slea.readInt();
         int gender = slea.readByte();
 
-        if (ServerConstants.USE_CUSTOM_CLIENT) {
-            // TODO: Add check for invalid items
-        } else {
-            int[] items = new int[]{weapon, top, bottom, shoes, hairstyle, face};
+        int[] items = new int[]{weapon, top, bottom, shoes, hair, face};
 
-            for (int i = 0; i < items.length; i++) {
-                if (!isLegal(items[i])) {
-                    FilePrinter.printError(FilePrinter.EXPLOITS + name + ".txt", "Owner from account '" + c.getAccountName() + "' tried to packet edit in char creation.");
-                    c.disconnect(true, false);
-                    return;
+        int[] legitM = new int[]{1302000, 1040002, 1060002, 1072005, 30020, 20401};
+        int[] legitF = new int[]{1302000, 1041010, 1061008, 1072005, 31000, 21001};
+
+        int[] legitAranM = new int[]{1442079, 1042167, 1062115, 1072383, 30020, 20401};
+        int[] legitAranF = new int[]{1442079, 1042167, 1062115, 1072383, 31000, 21001};
+
+        for (int i = 0; i < items.length; i++) {
+            if (!isLegal(items[i])) {
+                int replacementItem;
+
+                // Male
+                if (gender == 0) {
+                    if (job == 2) {
+                        replacementItem = legitAranM[i];
+                    } else {
+                        replacementItem = legitM[i];
+                    }
+                } else {
+                    if (job == 2) {
+                        replacementItem = legitAranF[i];
+                    } else {
+                        replacementItem = legitF[i];
+                    }
                 }
+
+                switch (i) {
+                    case 0:
+                        weapon = replacementItem;
+                        break;
+                    case 1:
+                        top = replacementItem;
+                        break;
+                    case 2:
+                        bottom = replacementItem;
+                        break;
+                    case 3:
+                        shoes = replacementItem;
+                        break;
+                    case 4:
+                        hair = replacementItem;
+                        break;
+                    case 5:
+                        face = replacementItem;
+                        break;
+                }
+
+                // TODO: Add this back once custom client's items are in sync with server
+                //FilePrinter.printError(FilePrinter.EXPLOITS + name + ".txt", "Owner from account '" + c.getAccountName() + "' tried to packet edit in char creation.");
+                //c.disconnect(true, false);
+                //return;
             }
         }
 
